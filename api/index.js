6a7009235a45a48d2f3f9dda3f99ec2a28c37a45
JOSHNA SENTHIL <joesenthil07@gmail.com>
2026-02-19 22:38:22 +0530

    Fix Vercel JSON encoding and add serverless config

---
+++ api/index.js
@@ -0,0 +1,7 @@
+﻿import app from "../src/app.js";
+import { connectDatabase } from "../src/config/db.js";
+
+export default async function handler(req, res) {
+  await connectDatabase();
+  return app(req, res);
+}
--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,14 @@
 ﻿import mongoose from "mongoose";
 import { env } from "./env.js";
 
+let isConnected = false;
+
 export const connectDatabase = async () => {
+  if (isConnected) {
+    return;
+  }
+
   await mongoose.connect(env.mongoUri);
+  isConnected = true;
   console.log("MongoDB connected");
 };
